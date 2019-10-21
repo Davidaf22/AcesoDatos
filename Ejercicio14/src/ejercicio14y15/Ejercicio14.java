d1367ac5228f575fe18983e4015c1fdfdc3931a3
oracle <oracle@localhost>
2019-10-21 13:57:09 +0200

    La rosalia

--- Ejercicio14/src/ejercicio14y15/Ejercicio14.java
+++ Ejercicio14/src/ejercicio14y15/Ejercicio14.java
@@ -6,6 +6,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLOutputFactory;
+import javax.xml.stream.XMLStreamConstants;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
 import javax.xml.stream.XMLStreamWriter;
@@ -15,7 +16,7 @@ public class Ejercicio14 {
     public static void main(String[] args) throws IOException, XMLStreamException {
         
         XMLOutputFactory a4 = XMLOutputFactory.newInstance();
-        XMLStreamWriter a5 = a4.createXMLStreamWriter(new  FileWriter (new File ("exercicio14.txt")));
+        XMLStreamWriter a5 = a4.createXMLStreamWriter(new  FileWriter (new File ("exercicio14.xml")));
         a5.writeStartDocument("1.0");
         a5.writeStartElement("autores");
         a5.writeStartElement("autor");
@@ -47,14 +48,22 @@ public class Ejercicio14 {
         a5.close();
         
         XMLInputFactory pol = XMLInputFactory.newInstance();
-        XMLStreamReader lon = pol.createXMLStreamReader(new  FileReader (new File ("exercicio14.txt")));
+        XMLStreamReader lon = pol.createXMLStreamReader(new  FileReader (new File ("exercicio14.xml")));
                 
-        do{
-            System.out.println(lon.next());
-            System.out.println(lon.getLocalName());
-            System.out.println(lon.getAttributeValue(1));
-            System.out.println(lon.getElementText());
-        }while(lon.hasNext());
+        while(lon.hasNext()){
+            if(lon.getEventType()==XMLStreamConstants.START_ELEMENT){
+                if("autor".equals(lon.getLocalName())){
+                   String atributo = lon.getAttributeValue(0);
+                  // String nombre = lon.getElementText();
+                   System.out.println(atributo+" ");
+                }
+                else if ("titulo".equals(lon.getLocalName()) || "nome".equals(lon.getLocalName())) {
+                    String nombre = lon.getElementText();
+                    System.out.println(" "+nombre);
+                }
+            }
+            lon.next();
+        }
         
     }
     
